6efe0c0b2564cdb7c19b0bb6947331088419b61d
Francisco Merchan <merchanf6@gmail.com>
2023-12-22 22:43:38 -0300

    Component extend styles listo

--- src/02-components-patterns/pages/ShoppingPage.tsx
+++ src/02-components-patterns/pages/ShoppingPage.tsx
@@ -2,10 +2,12 @@ import { ProductButtons } from "../components/ProductButtons";
 import { ProductCard } from "../components/ProductCard";
 import { ProductImage } from "../components/ProductImage";
 import { ProductTitle } from "../components/ProductTitle";
+import "../styles/custon-styles.css";
 
 const product = {
   id: "1",
   title: "Coffe Mug - Card",
+  img: "../../../public/coffee-mug.png",
 };
 
 export const ShoppingPage = () => {
@@ -20,15 +22,31 @@ export const ShoppingPage = () => {
           flexWrap: "wrap",
         }}
       ></div>
-      {/* <ProductCard product={product}>
-        <ProductCard.Image />
-        <ProductCard.Title title={"Cafe"} />
-        <ProductCard.Buttons />
-      </ProductCard> */}
-      <ProductCard product={product}>
-        <ProductImage />
-        <ProductTitle title={"Cafe"} />
-        <ProductButtons />
+
+      <ProductCard product={product} className="bg-dark text-white">
+        <ProductImage className="custom-image" />
+        <ProductTitle title={"Cafe"} className="text-bold" />
+        <ProductButtons className="custom-buttons" />
+      </ProductCard>
+
+      <ProductCard
+        product={product}
+        style={{
+          backgroundColor: "#70D1F8",
+        }}
+      >
+        <ProductImage
+          style={{
+            boxShadow: "10px 10px 10px rgba(0,0,0,0.5)",
+          }}
+        />
+        <ProductTitle style={{ fontWeight: "bold" }} />
+        <ProductButtons
+          style={{
+            display: "flex",
+            justifyContent: "end",
+          }}
+        />
       </ProductCard>
     </div>
   );
